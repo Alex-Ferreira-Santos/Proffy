826d48ee3723ef079e038a43e8ced2514d7e7a55
Alex-Ferreira-Santos <alexfstos@gmail.com>
2020-08-07 11:21:55 -0300

    criando o insert

---
+++ NLW/src/database/createProffy.js
@@ -0,0 +1,19 @@
+module.exports = async function(db, {proffyValue, classValue, classScheduleValue}){
+    //inserir dados na table de teachers
+    const insertedProffy = await db.run(`
+        INSERT INTO proffys (
+            name,
+            avatar,
+            whatsapp,
+            bio
+        ) VALUES (
+            ${proffyValue.name},
+            ${proffyValue.avatar},
+            ${proffyValue.whatsapp},
+            ${proffyValue.bio}
+        );
+    `)
+
+
+    
+}
